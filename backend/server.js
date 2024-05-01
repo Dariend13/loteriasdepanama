6553490c43f6c660c381cfbe4fc0cafeed90a94f
Dariend13 <darienddelcid@gmail.com>
2024-05-01 00:31:37 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const passport = require('./config/passport');
 const http = require('http');
 const WebSocket = require('ws');
+const path = require('path'); // Asegúrate de importar 'path'
 
 dotenv.config();
 
@@ -22,30 +23,26 @@ mongoose.connect(process.env.MONGODB_URI)
 
 const allowedOrigins = ['http://localhost:3001', 'http://192.168.50.178:3005', 'http://192.168.50.177:3005', 'http://localhost:3000', 'http://34.165.92.44', 'http://192.168.56.1:3000', 'http://172.21.84.245:3000', 'http://172.21.85.77:3000', 'http://172.21.85.123:3005', 'http://172.20.80.1:3000', 'http://localhost:3005'];
 
-// Crear el servidor HTTP
 const server = http.createServer(app);
-
-// Crear el servidor WebSocket
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws) => {
   ws.on('message', (message) => {
     console.log('Received:', message);
   });
-
 });
 
 const userRoutes = require('./routes/userRoutes');
 const gameRoutes = require('./routes/gameRoutes')(wss);
-const dashboardRoutes = require('./routes/dashboardRoutes'); // Agrega el enrutador del dashboard
-const horoscopeRoutes = require('./routes/horoscopeRoutes')(wss);  // Asegúrate de que la ruta sea correcta
+const dashboardRoutes = require('./routes/dashboardRoutes');
+const horoscopeRoutes = require('./routes/horoscopeRoutes')(wss);
 const lottopega3Routes = require('./routes/LottoRoutes')(wss);
 
 app.use(cors({
   origin: function (origin, callback) {
     if (!origin) return callback(null, true);
     if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'La política CORS para este sitio no permite el acceso desde el origen especificado.DariendGay';
+      const msg = 'La política CORS para este sitio no permite el acceso desde el origen especificado.';
       console.log('Error:', msg);
       return callback(new Error(msg), false);
     }
@@ -58,15 +55,15 @@ app.use('/api/users', userRoutes);
 app.use('/api/games', gameRoutes);
 app.use('/api/horoscope', horoscopeRoutes);
 app.use('/api/lottopega3', lottopega3Routes);
-
-// Usa Passport para la autenticación
-app.use(passport.initialize()); 
-
-// Agrega el enrutador del dashboard
+app.use(passport.initialize());
 app.use('/dashboard', dashboardRoutes);
 
-app.get('/', (req, res) => {
-  res.send('Servidor funcionando');
+// Middleware para servir archivos estáticos del frontend de React
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+// Todas las rutas no manejadas se redirigen al frontend de React
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
 });
 
 server.listen(PORT, '0.0.0.0', () => {
